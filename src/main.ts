65253e2005af43f87e3adf0313602ca986daef11
Nicholas Sangoi Brutti <nicholassbrutti@gmail.com>
2021-09-22 23:30:58 -0300

    build: expose host env to heroku ci/cd

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(PORT);
+  await app.listen(PORT, HOST);
 }
 bootstrap();
